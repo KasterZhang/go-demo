305ca4143818f3ca12c4b116e10dbb5f669b56c8
zhangzijian <zjzhang@nreal.ai>
2020-10-27 12:59:00 +0800

    19

---
+++ leetcode/no_m_19.go
@@ -0,0 +1,38 @@
+package leetcode
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func removeNthFromEnd19(head *ListNode, n int) *ListNode {
+	result := head
+	var pre, tail *ListNode = result, result
+	tailCount := n - 1
+	preCount := n + 1
+	for head != nil {
+		if head == nil {
+			break
+		}
+		if tailCount == 0 {
+			tail = tail.Next
+		} else {
+			tailCount--
+		}
+		if preCount == 0 {
+			pre = pre.Next
+		} else {
+			preCount--
+		}
+		head = head.Next
+	}
+	if preCount != 0 {
+		return result.Next
+	} else {
+		pre.Next = tail
+	}
+
+	return result
+}
